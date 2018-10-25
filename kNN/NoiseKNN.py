5beb38c743541b1245d22903ac078381ed58dbcc
Alwaysproblem <reganyang0415@gmail.com>
2018-10-26 10:31:45 +1100

    add KNN

--- kNN/NoiseKNN.py
+++ kNN/NoiseKNN.py
@@ -3,15 +3,17 @@ from sklearn.neighbors import KNeighborsRegressor as knn
 from math import sqrt
 import cv2
 import numpy as np
+import time
 
-img = cv2.imread('back.jpg', 0)
+# img = cv2.imread('back.jpg', 0)
+img = cv2.imread('view.png', 0)
 # cv2.imshow('color_image', img)
 # cv2.waitKey(2000)
 
 # print(img[9])
 
 m, n = img.shape
-sampleRate = 0.50
+sampleRate = 0.3
 allsample=[]
 
 for i in range(m):
@@ -38,6 +40,7 @@ for i in range(len(sample_cord)):
     newimg[sample_cord[i]] = label[i]
 
 # print(newimg[9])
+cv2.imwrite(f'sampling_{round(sampleRate * 100)}.png', newimg)
 
 cv2.imshow("dd", newimg)
 cv2.waitKey(2000)
@@ -45,13 +48,19 @@ cv2.waitKey(2000)
 # X = [(1, 1), (2, 2), (3, 3), (4, 4)]
 # y = [1, 2, 3, 4]
 
+start = time.clock()
 neigh = knn(n_neighbors=30, weights="distance")
 neigh.fit(sample_cord, label)
 
+end = time.clock()
 for row in range(m):
     for col in range(n):
         if (row, col) not in sample_cord:
             newimg[row, col] = neigh.predict([(row, col)])
+
+print(f"the MSE is {np.linalg.norm(newimg - img)/(m * n)}")
+
+print(f"the run time is {end - start}")
 
 cv2.imwrite('result.png', newimg)
 cv2.imshow('new', newimg)
